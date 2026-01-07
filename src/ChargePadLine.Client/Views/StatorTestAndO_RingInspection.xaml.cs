1ba70b303579a7bdd6b41553566a1cf7bd2d01b7
向阳 姚 <314742885@qq.com>
2026-01-07 09:26:03 +0800

    添加标题bar

---
+++ src/ChargePadLine.Client/Views/StatorTestAndO_RingInspection.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChargePadLine.Client.Views
+{
+    /// <summary>
+    /// StatorTestAndO_RingInspection.xaml 的交互逻辑
+    /// </summary>
+    public partial class StatorTestAndO_RingInspection : UserControl
+    {
+        public StatorTestAndO_RingInspection()
+        {
+            InitializeComponent();
+        }
+    }
+}
