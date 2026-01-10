254ed3ca7f830848d353d9326cecab5baf0ddefc
yangzhichao096 <2603555614@qq.com>
2026-01-10 16:28:58 +0800

    plc2

--- src/ChargePadLine.Client/Services/PlcService/Plc2/导热胶涂敷/导热胶涂敷ExitModel.cs
+++ src/ChargePadLine.Client/Services/PlcService/Plc2/导热胶涂敷/导热胶涂敷ExitModel.cs
@@ -10,8 +10,8 @@ namespace ChargePadLine.Client.Services.PlcService.Plc2
     {
         private bool _req;
         private bool _resp;
-        private bool _enterOk;
-        private bool _enterNg;
+        private bool _exitOk;
+        private bool _exitNg;
         private string _sn = string.Empty;
 
         /// <summary>
@@ -48,12 +48,12 @@ namespace ChargePadLine.Client.Services.PlcService.Plc2
 
         public bool 导热胶ExitNg
         {
-            get => _enterNg;
+            get => _exitNg;
             private set
             {
-                if (_enterNg != value)
+                if (_exitNg != value)
                 {
-                    _enterNg = value;
+                    _exitNg = value;
                     OnPropertyChanged(nameof(导热胶ExitNg));
                 }
             }
@@ -62,12 +62,12 @@ namespace ChargePadLine.Client.Services.PlcService.Plc2
 
         public bool 导热胶ExitOk
         {
-            get => _enterOk;
+            get => _exitOk;
             private set
             {
-                if (_enterOk != value)
+                if (_exitOk != value)
                 {
-                    _enterOk = value;
+                    _exitOk = value;
                     OnPropertyChanged(nameof(导热胶ExitOk));
                 }
             }
