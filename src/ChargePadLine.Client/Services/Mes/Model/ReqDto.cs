8960a32867ccaae018c186abac154f9c18d4eec2
向阳 姚 <314742885@qq.com>
2026-01-13 09:49:08 +0800

    上传测试项

--- src/ChargePadLine.Client/Services/Mes/Model/ReqDto.cs
+++ src/ChargePadLine.Client/Services/Mes/Model/ReqDto.cs
@@ -13,7 +13,18 @@ namespace ChargePadLine.Client.Services.Mes.Model
         public string stationCode { get; set; } = string.Empty;
         public string? workOrderCode { get; set; } = string.Empty;
         public string? testResult { get; set; } = string.Empty;
-        public string? testData { get; set; } = string.Empty;
+        public List<TestDataItem>? testData { get; set; } = new List<TestDataItem>();
 
+    }
+
+    public class TestDataItem
+    {
+        public float? Upperlimit { get; set; }
+        public float? Lowerlimit { get; set; }
+        public string? Units { get; set; } = string.Empty;
+        public string? ParametricKey { get; set; } = string.Empty;
+        public string? TestResult { get; set; } = string.Empty;
+        public string? TestValue { get; set; } = string.Empty;
+        public string? Remark { get; set; } = string.Empty;       
     }
 }
