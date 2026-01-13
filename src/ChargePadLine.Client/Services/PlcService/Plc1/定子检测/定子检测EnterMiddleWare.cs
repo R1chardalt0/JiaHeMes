40fff006068d2ad5cb2d233bd30d050730f60f7d
向阳 姚 <314742885@qq.com>
2026-01-13 11:02:07 +0800

    Station1添加过站校验接口

--- src/ChargePadLine.Client/Services/PlcService/Plc1/定子检测/定子检测EnterMiddleWare.cs
+++ src/ChargePadLine.Client/Services/PlcService/Plc1/定子检测/定子检测EnterMiddleWare.cs
@@ -1,9 +1,13 @@
+using ChargePadLine.Client.Controls;
 using ChargePadLine.Client.Helpers;
+using ChargePadLine.Client.Services.Mes;
+using ChargePadLine.Client.Services.Mes.Model;
+using ChargePadLine.Client.Services.PlcService;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using ChargePadLine.Client.Services.PlcService;
 
 namespace ChargePadLine.Client.Services.PlcService.Plc1.定子检测
 {
@@ -15,12 +19,16 @@ namespace ChargePadLine.Client.Services.PlcService.Plc1.定子检测
         private readonly ILogger<定子检测EnterMiddleWare> _logger;
         private readonly StatorEnterModel _statorTestDataService;
         private readonly ILogService _logService;
+        private readonly StationConfig _stationconfig;
+        private readonly IMesApiService _mesApi;
 
-        public 定子检测EnterMiddleWare(ILogger<定子检测EnterMiddleWare> logger, StatorEnterModel statorTestDataService, ILogService logService)
+        public 定子检测EnterMiddleWare(ILogger<定子检测EnterMiddleWare> logger, StatorEnterModel statorTestDataService, ILogService logService, IOptions<StationConfig> stationconfig, IMesApiService mesApi)
         {
             _logger = logger;
             _statorTestDataService = statorTestDataService;
             _logService = logService;
+            _stationconfig = stationconfig.Value;
+            _mesApi = mesApi;
         }
 
         private bool _isInitialized = false;
@@ -31,11 +39,11 @@ namespace ChargePadLine.Client.Services.PlcService.Plc1.定子检测
             {
                 string statusMessage = "";
                 //plc状态读取
-                var malfunction= s7Net.ReadBool("DB4010.4.0").Content;//设备故障
-                var auto= s7Net.ReadBool("DB4010.4.1").Content;//自动模式
-                var idle= s7Net.ReadBool("DB4010.4.2").Content;//设备空闲
-                var manual= s7Net.ReadBool("DB4010.4.3").Content;//手动模式
-                var check= s7Net.ReadBool("DB4010.4.4").Content;//审核模式
+                var malfunction = s7Net.ReadBool("DB4010.4.0").Content;//设备故障
+                var auto = s7Net.ReadBool("DB4010.4.1").Content;//自动模式
+                var idle = s7Net.ReadBool("DB4010.4.2").Content;//设备空闲
+                var manual = s7Net.ReadBool("DB4010.4.3").Content;//手动模式
+                var check = s7Net.ReadBool("DB4010.4.4").Content;//审核模式
 
                 if (malfunction) statusMessage = "设备故障";
                 else if (auto) statusMessage = "自动模式";
@@ -50,16 +58,36 @@ namespace ChargePadLine.Client.Services.PlcService.Plc1.定子检测
                 var resp = s7Net.ReadBool("DB4010.10.0").Content;
                 var enterok = s7Net.ReadBool("DB4010.2.0").Content;//进站OK
                 var enterng = s7Net.ReadBool("DB4010.2.1").Content;//进站NG
-                var sn = s7Net.ReadString("DB4010.200",100).Content.Trim().Replace("\0", "").Replace("\b", "");
+                var sn = s7Net.ReadString("DB4010.200", 100).Content.Trim().Replace("\0", "").Replace("\b", "");
 
                 // 更新数据服务
-                _statorTestDataService.UpdateData(req, resp, sn,enterok,enterng, statusMessage);
+                _statorTestDataService.UpdateData(req, resp, sn, enterok, enterng, statusMessage);
 
                 if (req && !resp)
                 {
                     await _logService.RecordLogAsync(LogLevel.Information, "定子检测进站请求收到");
-                    s7Net.Write("DB4010.10.0", true);
-                    s7Net.Write("DB4010.2.0", true);
+
+                    var reqParam = new ReqDto
+                    {
+                        sn = sn,
+                        resource = _stationconfig.Station1.Resource,
+                        stationCode = _stationconfig.Station1.StationCode,
+                        workOrderCode = _stationconfig.Station1.WorkOrderCode
+                    };
+                    var res = await _mesApi.UploadCheck(reqParam);
+                    if (res.code == 0)
+                    {
+                        s7Net.Write("DB4010.10.0", true);
+                        s7Net.Write("DB4010.2.0", true);
+                        await _logService.RecordLogAsync(LogLevel.Information, "定子检测进站校验成功");
+                    }
+                    else
+                    {
+                        s7Net.Write("DB4010.10.0", true);
+                        s7Net.Write("DB4010.2.1", true);
+                        await _logService.RecordLogAsync(LogLevel.Information, $"定子检测进站校验失败，mes返回:{res.message}");
+                    }
+
                 }
                 else if (!req && resp)
                 {
@@ -68,7 +96,7 @@ namespace ChargePadLine.Client.Services.PlcService.Plc1.定子检测
                     s7Net.Write("DB4010.2.1", false);
                     await _logService.RecordLogAsync(LogLevel.Information, "定子检测进站请求复位");
                 }
-                
+
                 await Task.CompletedTask;
             }
             catch (Exception ex)
