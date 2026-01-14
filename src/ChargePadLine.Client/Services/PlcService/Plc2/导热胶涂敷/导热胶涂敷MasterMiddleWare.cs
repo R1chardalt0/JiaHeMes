1e6da68c1971ee5346605e217932d3ee5f479b0d
yangzhichao096 <2603555614@qq.com>
2026-01-14 14:00:52 +0800

    添加plc1-9点检

---
+++ src/ChargePadLine.Client/Services/PlcService/Plc2/导热胶涂敷/导热胶涂敷MasterMiddleWare.cs
@@ -0,0 +1,115 @@
+﻿using ChargePadLine.Client.Controls;
+using ChargePadLine.Client.Helpers;
+using ChargePadLine.Client.Services.Mes;
+using ChargePadLine.Client.Services.Mes.Dto;
+using ChargePadLine.Client.Services.PlcService.Plc1.O型圈及冷却铝板装配;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargePadLine.Client.Services.PlcService.Plc2.导热胶涂敷
+{
+    public class 导热胶涂敷MasterMiddleWare: IPlc2Task
+    {
+        private readonly ILogger<导热胶涂敷MasterMiddleWare> _logger;
+        private readonly ILogService _logService;
+        private readonly StationConfig _stationconfig;
+        private readonly IMesApiService _mesApi;
+        private const string PlcName = "【导热胶涂敷】";
+        private List<TestDataItem> testDatas = new List<TestDataItem>();
+
+        public 导热胶涂敷MasterMiddleWare(ILogger<导热胶涂敷MasterMiddleWare> logger, ILogService logService, IOptions<StationConfig> stationconfig, IMesApiService mesApi)
+        {
+            _logger = logger;
+            _logService = logService;
+            _stationconfig = stationconfig.Value;
+            _mesApi = mesApi;
+        }
+
+        public async Task ExecuteOnceAsync(S7NetConnect s7Net, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var req = s7Net.ReadBool("DB4020.7.0").Content;
+                var resp = s7Net.ReadBool("DB4020.14.0").Content;
+                var enterok = s7Net.ReadBool("DB4020.3.0").Content;//出站OK
+                var enterng = s7Net.ReadBool("DB4020.3.1").Content;//出站NG
+                var sn = s7Net.ReadString("DB4020.66.0", 100).Content.Trim().Replace("\0", "").Replace("\b", "");
+
+                // 更新数据服务
+                //_statorTestDataService.UpdateData(req, resp, sn, enterok, enterng);
+
+                if (req && !resp)
+                {
+                    var isok = s7Net.ReadBool("DB4020.16.0").Content;
+
+                    await _logService.RecordLogAsync(LogLevel.Information, $"{PlcName}点检请求收到");
+
+                    testDatas = new List<TestDataItem>()
+                    {
+                        new TestDataItem
+                        {
+                            ParametricKey = "绝缘电阻",
+                            TestValue = "",
+                            Units = "MΩ",
+                            Upperlimit = 1000,
+                            Lowerlimit = 50,
+                            TestResult = "Pass",
+                            Remark = ""
+                        },
+                        new TestDataItem
+                        {
+                            ParametricKey = "耐压测试",
+                            TestValue = "",
+                            Units = "V",
+                            Upperlimit = 2000,
+                            Lowerlimit = 1500,
+                            TestResult ="",
+                            Remark = ""
+                        }
+                    };
+
+                    var reqParam = new ReqDto
+                    {
+                        sn = sn,
+                        resource = _stationconfig.Station1.Resource,
+                        stationCode = _stationconfig.Station1.StationCode,
+                        workOrderCode = _stationconfig.Station1.WorkOrderCode,
+                        testResult = isok ? "Pass" : "Fail",
+                        testData = testDatas
+                    };
+                    var res = await _mesApi.UploadData(reqParam);
+                    if (res.code == 0)
+                    {
+                        s7Net.Write("DB4020.14.0", true);
+                        s7Net.Write("DB4020.3.0", true);
+                        await _logService.RecordLogAsync(LogLevel.Information, $"{PlcName}点检收集完成");
+                    }
+                    else
+                    {
+                        s7Net.Write("DB4020.14.0", true);
+                        s7Net.Write("DB4020.3.1", true);
+                        await _logService.RecordLogAsync(LogLevel.Information, $"{PlcName}点检收集失败，mes返回:{res.message}");
+                    }
+                }
+                else if (!req && resp)
+                {
+                    s7Net.Write("DB4020.14.0", false);
+                    s7Net.Write("DB4020.3.0", false);
+                    s7Net.Write("DB4020.3.1", false);
+                    await _logService.RecordLogAsync(LogLevel.Information, $"{PlcName}点检请求复位");
+                }
+
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                await _logService.RecordLogAsync(LogLevel.Error, $"{PlcName}点检MiddleWare异常: {ex.Message}");
+            }
+        }
+    }
+}
